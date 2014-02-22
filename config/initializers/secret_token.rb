00c5969d1e6e4a13e0d2e7e1c3324f8f782712ae
Jared Buckner <jbuckner@wisc.edu>
2014-02-21 23:33:58 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CongressViz::Application.config.secret_key_base = '0d5775265f8d2d93a6b5e723523aa84a0d7ab1426bd142e02d6c9009faeb32ad72fdb39297ada93b6dbe44c5ca49320100eb79b5947c33505e4f5e11dc322be8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CongressViz::Application.config.session_store :cookie_store, key: '_congress-viz_session'
